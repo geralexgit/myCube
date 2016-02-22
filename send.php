c036a1f084c17121944c6649260b3960f05218c8
geralexgit <alexandgeras@gmail.com>
2016-02-22 03:38:10 +0300

    скрипт отпарвляет сообщения
    скрипт отпарвляет сообщения

--- send.php
+++ send.php
@@ -5,29 +5,12 @@ $botToken = "173479491:AAFr7YjQw1qxTVkjuqG0itjBHJCEFAjG36o";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $chatId = 21439948;
-
+//получаем поля
 $service = $_GET['service'];
 $name = $_GET['name'];
-$phone = $GET['phone'];
-$message = $GET['message'];
-
-$fmessage = "'Тип задачи: ' + .$service. '. Имя обратившегося: ' .$name.  '. Номер телефона: ' .$phone. '. Текст сообщения: ' + .$message.";
-
-//$fmessage = utf8_encode($fmessage);
-echo "$fmessage"; 
-
-
-
-//$messageToSend=$name+$phone+$service+$message;
-//$messageToSend = $_POST['myMessage'];
-//$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
-//print_r($chatId);
-
-//print_r($message);
-//echo $message;
-
-                    
-//Текст нащего сообщения можно использовать HTML теги
-file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$service);
+$phone = $_GET['phone'];
+$message = $_GET['message'];
+//отправляем поля
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$service. ". ".$name.". ".$phone.". ".$message);
 
 ?>
